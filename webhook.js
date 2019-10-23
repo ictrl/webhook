f5adf7e43141c14a09681d4d27300820677c6d3e
Samrat Saurav Jaiswal <coolsaurav34@gmail.com>
2019-10-23 14:46:30 +0530

    sms

--- webhook.js
+++ webhook.js
@@ -192,7 +192,7 @@ app.post("/", function(request, response) {
   message = `MojitoLabs:%20Hi%20${name},%20Thanks%20for%20shopping%20with%20us!%20Your%20order%20is%20confirmed,%20and%20will%20be%20shipped%20shortly.%20Your%20order%20ID:%20${orderId}`;
 
   console.log("phone--->", phone);
-  // sndSms(phone, message);
+  sndSms(phone, message);
 });
 
 app.listen(process.env.PORT || 4000, () => {
