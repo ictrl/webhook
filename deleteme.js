7abe489a61471ee0afe521d6bbe013adca3857c8
Samrat Saurav Jaiswal <coolsaurav34@gmail.com>
2019-11-09 11:38:04 +0530

    data.template undefined

--- deleteme.js
+++ deleteme.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const shop = "mojitostore.myshopify.com";
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -15,23 +16,65 @@ const shopSchema = new mongoose.Schema({
 });
 const Store = new mongoose.model("Store", shopSchema);
 
-Store.findOne({ name: "mojitostore.myshopify.com" }, function(err, data) {
-  let s = data.smsCount;
-  console.log("-->", s);
-  Store.findOneAndUpdate(
-    { name: "mojitostore.myshopify.com" },
-    {
-      $set: {
-        smsCount: data.smsCount - 2
-      }
-    },
-    { new: true, useFindAndModify: false },
-    (err, data) => {
-      if (!err) {
-        console.log("data");
-      } else {
-        console.log("err", err);
-      }
+Store.findOne({ name: shop }, function(err, data) {
+  if (!err) {
+    if (data.data["orders/create customer"] != undefined) {
+      console.log(data);
+      
+      // data.template.forEach(element => {
+      //   console.log("in");
+      //   if (element.topic === topic) {
+      //     console.log("in1");
+      //     if (element.customer) {
+      //       message = element.customer;
+      //       console.log("in2");
+      //     } else {
+      //       console.log("else1");
+
+      //       message = `Hi%20${name},%20Thanks%20for%20shopping%20with%20us!%20Your%20order%20is%20confirmed,%20and%20will%20be%20shipped%20shortly.%20Your%20order%20ID:%20${orderId}`;
+      //     }
+      //   } else {
+      //     console.log("else2");
+
+      //     message = `Hi%20${name},%20Thanks%20for%20shopping%20with%20us!%20Your%20order%20is%20confirmed,%20and%20will%20be%20shipped%20shortly.%20Your%20order%20ID:%20${orderId}`;
+      //   }
+      // });
     }
-  );
+  }
 });
+
+// var message = "";
+// var data = {
+//   sms: [],
+//   template: [],
+//   name: "mojitostore.myshopify.com",
+//   data: {
+//     "admin no": "9898989898",
+//     "orders/create customer": "on",
+//     "orders/create admin": "on",
+//     "sender id": "MOJITO"
+//   },
+//   smsCount: 6099,
+//   __v: 0
+// };
+
+// data.template.forEach(element => {
+//   console.log("in");
+//   if (element.topic === topic) {
+//     console.log("in");
+//     if (element.customer) {
+//       message = element.customer;
+//       console.log("in2");
+//     } else {
+//       console.log("else1");
+
+//       message = `Hi%20${name},%20Thanks%20for%20shopping%20with%20us!%20Your%20order%20is%20confirmed,%20and%20will%20be%20shipped%20shortly.%20Your%20order%20ID:%20${orderId}`;
+//     }
+//   } else {
+//     console.log("else2");
+
+//     message = `Hi%20${name},%20Thanks%20for%20shopping%20with%20us!%20Your%20order%20is%20confirmed,%20and%20will%20be%20shipped%20shortly.%20Your%20order%20ID:%20${orderId}`;
+//   }
+// });
+
+// console.log(message);
