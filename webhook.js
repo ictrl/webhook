daeb48e6b51aee53aef253923097b66964db9614
Aditya Kumar Jha <mail@adijha.com>
2020-08-07 19:38:09 +0545

    sma api ashish

--- webhook.js
+++ webhook.js
@@ -1174,13 +1174,23 @@ const sndSms = (phone, message, senderID, shop) => {
 				let LeftSMS = data.smsCount - data.sms.length;
 				if (LeftSMS > 0) {
 					//send SMS
-					 var options = {
-            method: "GET",
-            hostname: "api.datagenit.com",
-            port: null,
-            path: `/sms?auth=${smsapi}&msisdn=${phone}&senderid=${senderID}&message=${message}`,
-            headers: {},
-          };
+					//  var options = {
+          //   method: "GET",
+          //   hostname: "api.datagenit.com",
+          //   port: null,
+          //   path: `/sms?auth=${smsapi}&msisdn=${phone}&senderid=${senderID}&message=${message}`,
+          //   headers: {},
+					// };
+					var options = {
+						method: 'GET',
+						url: 'https://global.datagenit.com/API/sms-api.php',
+						qs:
+						 { auth: smsapi,
+							 senderid: senderID,
+							 msisdn: phone,
+							 message: message },
+						headers:
+						 {'cache-control': 'no-cache' } };
 					try {
 						var req = http.request(options, function(res) {
 							var chunks = [];
@@ -1798,12 +1808,14 @@ cron.schedule('*/2 * * * * ', async () => {
 	}
 });
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
-}
+// }
+
+
 app.post('/whatsapp', function(req, res) {
 	res.sendStatus(200);
 	console.log(req.body, 'whatsapp response');
