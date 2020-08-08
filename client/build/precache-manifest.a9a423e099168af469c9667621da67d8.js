f4b3ea3cea8ad56e2e65eafced525d4405a0d01b
Aditya Kumar Jha <mail@adijha.com>
2020-08-08 18:48:07 +0545

    updated

---
+++ client/build/precache-manifest.a9a423e099168af469c9667621da67d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01fb81f5c5216d5919d971a52442955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e73e744b34048a0fe6d",
+    "url": "/static/css/2.d24c2f25.chunk.css"
+  },
+  {
+    "revision": "05d018482211b64b9f67",
+    "url": "/static/css/main.81efa1c6.chunk.css"
+  },
+  {
+    "revision": "1e73e744b34048a0fe6d",
+    "url": "/static/js/2.9536bc77.chunk.js"
+  },
+  {
+    "revision": "05d018482211b64b9f67",
+    "url": "/static/js/main.fa7927e6.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
